80addf176845ed95bccf06651865f38c6ef4612d
PhilNel <phil15634280@gmail.com>
2026-01-10 15:11:14 +0200

    refactor: Split Nemesis references up and add remaining citations

---
+++ internal/models/references_nemesis_decks.go
@@ -0,0 +1,142 @@
+package models
+
+func getNemesisDeckReferences() []Reference {
+	return []Reference{
+		// Attack Deck
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "ATK1",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Intruder Attack Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		// Serious Wound Deck
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "WND1",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Serious Wound Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		// General Character Action
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "CHARACTION",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Character Action Decks",
+			PageReference: "",
+			URL:           "",
+		},
+		// Event Deck
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "EVT1",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Event Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		// Intruder Player Action Deck
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "INT1",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Intruder Player Action Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		// Solo/Coop Objectives Deck
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "OBJ-COOP",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Solo/Coop Objectives Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		// Item Decks
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "ITM1-BLUE",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Craft (Blue) Item Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "ITM1-GREEN",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Medical (Green) Item Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "ITM1-RED",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Military (Red) Item Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "ITM1-YELLOW",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Technical (Yellow) Item Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		// Intruder Weakness Deck
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "WEAK1",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Intruder Weakness Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		// Objective Decks
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "OBJ-PERSONAL",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Personal Objectives Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "OBJ-CORPORATE",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Corporate Objectives Deck",
+			PageReference: "",
+			URL:           "",
+		},
+		// Coordinates Deck
+		{
+			GameId:        "nemesis",
+			ReferenceId:   "COORD1",
+			Type:          "deck",
+			Title:         "Nemesis Core Game",
+			Section:       "Coordinates Deck",
+			PageReference: "",
+			URL:           "",
+		},
+	}
+}
